0ce9597443956e0ffa0d152e55ff1125e773c1d4
dblock <dblock@amazon.com>
2024-10-20 09:41:08 -0400

    Clarify help.

--- slack-api-explorer/commands/help.rb
+++ slack-api-explorer/commands/help.rb
@@ -18,6 +18,9 @@ module SlackApiExplorer
         Commands
         --------
         #{`slack help`.gsub(/.*?(COMMANDS)/m, '').gsub(/^(    )/, '')}
+
+        Not sure where to start? Try "auth test", then invite the bot to #general and DM it "chat postMessage --as_user=true --text='Hello World' --channel=#general".
+
         Most commands contain subcommands, try "help <command>" (eg. "help api") to get a list of subcommands.
         ```
       EOS
