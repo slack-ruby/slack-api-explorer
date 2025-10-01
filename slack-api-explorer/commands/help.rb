951f2304c74947393e5bf1a13acedfba49e64cce
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-10-01 09:11:35 -0400

    Fixed help, markdown too long.

--- slack-api-explorer/commands/help.rb
+++ slack-api-explorer/commands/help.rb
@@ -6,6 +6,25 @@ module SlackApiExplorer
         @help[expression] ||= '```' + `slack help #{expression}`.gsub(/^(    )/, '') + '```'
       end
 
+      def self.commands
+        @commands ||= begin
+          commands = `slack help`.gsub(/.*?(COMMANDS)/m, '').gsub(/^(    )/, '')
+
+          all = {}
+          commands.split("\n").each { |command|
+            next if command.strip.blank?
+
+            parts = command.split(/[\_\s]/, 2)
+            all[parts[0]] ||= []
+            all[parts[0]] << parts[1]
+          }
+
+          all.keys.map do |group|
+            "#{group}#{all[group].first}"
+          end
+        end
+      end
+
       HELP = <<~EOS.freeze
         I am your friendly Api Explorer, here to help.
 
@@ -17,12 +36,12 @@ module SlackApiExplorer
 
         Commands
         --------
-        #{`slack help`.gsub(/.*?(COMMANDS)/m, '').gsub(/^(    )/, '')}
+        #{commands.join("\n")}
 
         Not sure where to start? Try "auth test", then invite the bot to #general and DM it "chat postMessage --as_user=true --text='Hello World' --channel=#general".
 
         Most commands contain subcommands, try "help <command>" (eg. "help api") to get a list of subcommands.
-        ```\n
+        ```
       EOS
 
       def self.call(client, data, match)
