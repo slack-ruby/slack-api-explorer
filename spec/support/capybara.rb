981a938bd9222fbdaa44b07a95b7d7af2cd5925c
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-10-01 08:35:05 -0400

    Put back unicorn.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,5 +3,5 @@ require 'capybara/rspec'
 Capybara.configure do |config|
   config.app = Api::Middleware.instance
   config.server_port = 9293
-  config.server = :puma
+  config.server = :webrick
 end
